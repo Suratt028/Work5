73ed558bf49b598099ab6cad777462eed5a3ab50
Suratt028 <154447222+Suratt028@users.noreply.github.com>
2024-02-03 21:56:39 +0700

    Create TestColour.java

---
+++ TestColour.java
@@ -0,0 +1,15 @@
+public class TestColour {
+    public static void main(String[] args) {
+        Colour ojb1 = new Colour();
+        ojb1.setRed(1);
+        System.out.println(ojb1.getRed());
+
+        Colour obj2 = new Colour();
+        obj2.setBlue(2);
+        System.out.println(obj2.getBlue());
+
+        Colour obj3 = new Colour();
+        obj3.setGreen(3);
+        System.out.println(obj3.getGreen());
+    }
+}
