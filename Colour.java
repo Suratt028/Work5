f7b2ce9d6a1a37eb2f027816b04bc9b36bf6b97c
Suratt028 <154447222+Suratt028@users.noreply.github.com>
2024-02-03 21:53:54 +0700

    Update Colour.java

--- Colour.java
+++ Colour.java
@@ -1,31 +1,40 @@
+
+
+
 public class Colour {
 
     private int red;
     private int green;
     private int blue;
 
+    public Colour() {
+        this.red = 0;
+        this.green = 0;
+        this.blue = 0;
+    }
     public int getRed() {
         return red;
     }
 
-    public void setRed(int r) {
-        this.red = r;
+    public void setRed(int red) {
+        this.red = red;
     }
 
     public int getGreen() {
         return green;
     }
 
-    public void setGreen(int g) {
-        this.green = g;
+    public void setGreen(int green) {
+        this.green = green;
     }
 
     public int getBlue() {
         return blue;
     }
 
-    public void setBlue(int b) {
-        this.blue = b;
+    public void setBlue(int blue) {
+        this.blue = blue;
     }
    
 }
+
