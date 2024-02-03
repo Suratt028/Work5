9c636d63bfb880ba1c6dfefc096820eb4d9104a6
Suratt028 <154447222+Suratt028@users.noreply.github.com>
2024-02-03 22:09:19 +0700

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,41 @@
+
+public class Item {
+    private String name;
+    private String id;
+    private String description;
+    private String preview;
+
+    public Item() {
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPreview() {
+        return preview;
+    }
+
+    public void setPreview(String preview) {
+        this.preview = preview;
+    }
+}
